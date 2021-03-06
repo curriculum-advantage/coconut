adf702254c698c4c66cc5ad43c84fb34aa15d405
Hunter Leeves <hunterleeves@gmail.com>
2021-03-05 23:42:05 -0600

    replaceMultiLabel1

--- src/utils/replaceMultiLabel/index.ts
+++ src/utils/replaceMultiLabel/index.ts
@@ -1,1 +1,12 @@
-export const replaceMultiLabel = 'hey';
+import MultiLabel from '../../nodes/sprites/MultiLabel';
+
+export const replaceMultiLabel = (label, text) => {
+  const { parent, ...labelProperties } = label;
+  const newMultiLabel = new MultiLabel({
+    parent,
+    defaultText: text,
+    ...labelProperties,
+  });
+  label.removeFromParent();
+  return newMultiLabel;
+};
