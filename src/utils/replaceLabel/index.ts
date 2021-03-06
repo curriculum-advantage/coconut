0c6d560e8a591a9eb7fd48c261e067a2610e90e5
Hunter Leeves <hunterleeves@gmail.com>
2021-03-05 23:20:56 -0600

    addingText

--- src/utils/replaceLabel/index.ts
+++ src/utils/replaceLabel/index.ts
@@ -1,22 +1,12 @@
 import createLabel from '../createLabel';
 
 export const replaceLabel = (label, text) => {
-  console.log('oldLabel:', label);
-  console.log('text:', text);
-  // how to remove current label?
-  // will have to grab off every single possible param here
   const { parent, ...labelProperties } = label;
-  console.log('file: index.ts ~ line 9 ~ labelProperties', labelProperties);
   const newLabel = createLabel({
+    text,
     parent,
     ...labelProperties,
-    fontSize: 20,
   });
-  // 1. get label's parent
-  console.log('file: index.ts ~ line 9 ~ parent', parent);
-  // 2. detach label from parent
   label.removeFromParent();
-  // 3. attach new label to label's parent
-  parent.addChild(newLabel);
   return newLabel;
 };
