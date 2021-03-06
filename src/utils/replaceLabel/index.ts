4e59512cf488f54c68a5c3813472dbb1c54fd293
Hunter Leeves <hunterleeves@gmail.com>
2021-03-05 21:55:50 -0600

    adding replaceLabel

---
+++ src/utils/replaceLabel/index.ts
@@ -0,0 +1,17 @@
+import createLabel from '../createLabel';
+
+export const replaceLabel = (label, text) => {
+  console.log('oldLabel:', label);
+  console.log('text:', text);
+  // how to remove current label?
+  // will have to grab off every single possible param here
+  const { ...labelProps } = label;
+  const newLabel = new createLabel({
+    ...labelProps,
+    fontSize: 30,
+  });
+  // 1. get label's parent
+  // 2. detach label from parent
+  // 3. attach new label to label's parent
+  return newLabel;
+};
