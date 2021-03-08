785064342bdfc8a00c280a6ef59a9bdda51380aa
Hunter Leeves <hunterleeves@gmail.com>
2021-03-08 15:42:37 -0600

    replaceLabel fix3

--- src/utils/replaceLabel/index.ts
+++ src/utils/replaceLabel/index.ts
@@ -1,9 +1,10 @@
 import createLabel from '../createLabel';
-//I can safely leave this to debug with some extra time Monday. It's super close. Just need the text to show up,
-//OR i will have to add the label to the parent in the actual file if that's the issue
+// I can safely leave this to debug with some extra time Monday. It's super close. Just need the text to show up,
+// OR i will have to add the label to the parent in the actual file if that's the issue
 
-export const replaceLabel = (label, text) => {
-  const { parent, ...labelProperties } = label;
+export const replaceLabel = (parent, label, text) => {
+  const { fontSize, ...labelProperties } = label;
+  console.log('labelProperties in coconut', labelProperties);
   const newLabel = createLabel({
     ...labelProperties,
     text,
