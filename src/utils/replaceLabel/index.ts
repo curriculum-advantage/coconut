32fd328b7c5429f8fa72fc9c71ba65ceab208d36
Hunter Leeves <hunterleeves@gmail.com>
2021-03-08 17:21:24 -0600

    createLabel13

--- src/utils/replaceLabel/index.ts
+++ src/utils/replaceLabel/index.ts
@@ -2,13 +2,42 @@ import createLabel from '../createLabel';
 // I can safely leave this to debug with some extra time Monday. It's super close. Just need the text to show up,
 // OR i will have to add the label to the parent in the actual file if that's the issue
 
-export const replaceLabel = (parent, label, text) => {
-  const { fontSize, ...labelProperties } = label;
-  console.log('labelProperties in coconut', labelProperties);
+export const replaceLabel = (newParent, label, newText) => {
+  // const { fontSize, ...labelProperties } = label;
+  const {
+    opacity,
+    fontName,
+    fontSize,
+    dimensions,
+    horizontalAlign,
+    verticalAlign,
+    fontWeight,
+    fontStyle,
+    position,
+    color,
+    strokeColor,
+    strokeSize,
+    anchor,
+    zOrder,
+  } = label;
+
   const newLabel = createLabel({
-    ...labelProperties,
-    text,
-    parent,
+    parent: newParent,
+    text: newText,
+    opacity,
+    fontName,
+    fontSize,
+    dimensions,
+    horizontalAlign,
+    verticalAlign,
+    fontWeight,
+    fontStyle,
+    position,
+    color,
+    strokeColor,
+    strokeSize,
+    anchor,
+    zOrder,
   });
   label.removeFromParent();
   return newLabel;
