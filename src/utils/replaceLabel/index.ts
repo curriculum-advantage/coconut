2865475bc09a185349fadaf97a84d5d4bff29b16
Hunter Leeves <hunterleeves@gmail.com>
2021-03-08 10:08:20 -0600

    testing multi1

--- src/utils/replaceLabel/index.ts
+++ src/utils/replaceLabel/index.ts
@@ -1,4 +1,6 @@
 import createLabel from '../createLabel';
+//I can safely leave this to debug with some extra time Monday. It's super close. Just need the text to show up,
+//OR i will have to add the label to the parent in the actual file if that's the issue
 
 export const replaceLabel = (label, text) => {
   const { parent, ...labelProperties } = label;
--- src/utils/replaceMultiLabel/index.ts
+++ src/utils/replaceMultiLabel/index.ts
@@ -1,12 +1,20 @@
 import MultiLabel from '../../nodes/sprites/MultiLabel';
+//I can safely leave this to debug with some extra time Monday. It's super close. Just need the text to show up,
+//OR i will have to add the label to the parent in the actual file if that's the issue
 
 export const replaceMultiLabel = (parent, label, text) => {
-  const { ...labelProperties } = label;
+  console.log('text', text);
+  console.log('label', label);
+  console.log('parent', parent);
+  const { fontSize, ...labelProperties } = label;
+  console.log('labelProperties', labelProperties);
   const newMultiLabel = new MultiLabel({
     defaultText: text,
     ...labelProperties,
   });
+  console.log(' newMultiLabel', newMultiLabel);
   label.removeFromParent();
   parent.addChild(newMultiLabel);
+  console.log('parentAfter', parent);
   return newMultiLabel;
 };
