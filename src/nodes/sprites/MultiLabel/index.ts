501293cb3233b6f63c11e5e0724f4754956c0588
Hunter Leeves <hunterleeves@gmail.com>
2021-03-08 10:18:28 -0600

    mutli props

--- src/nodes/sprites/MultiLabel/index.ts
+++ src/nodes/sprites/MultiLabel/index.ts
@@ -35,6 +35,23 @@ class MultiLabel extends cc.LayerColor {
     this.setContentSize(containerWidth, containerHeight);
     this.setPosition(...position);
 
+    this.anchor = anchor;
+    this.areaClick = areaClick;
+    this.clickHandler = clickHandler;
+    this.containerWidth = containerWidth;
+    this.containerHeight = containerHeight;
+    this.defaultFillIn = defaultFillIn;
+    this.fontColorHighlight = fontColorHighlight;
+    this.fontColorPrimary = fontColorPrimary;
+    this.fontSize = fontSize;
+    this.fontStyle = fontStyle;
+    this.fontWeight = fontWeight;
+    this.horizontalAlignment = horizontalAlignment;
+    this.lineHeight = lineHeight;
+    this.position = position;
+    this.wordSpace = wordSpace;
+    this.defaultText = defaultText;
+
     let displayedText;
     let numberReplacedFillIns = 0;
     const lineOffset = fontSize * lineHeight;
