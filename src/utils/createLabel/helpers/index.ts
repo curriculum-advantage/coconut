ac64c3a0d6c34905b56618cb26cef492577e632b
Hunter Leeves <hunterleeves@gmail.com>
2021-03-10 14:14:22 -0600

    syntax fixes

--- src/utils/createLabel/helpers/index.ts
+++ src/utils/createLabel/helpers/index.ts
@@ -8,9 +8,7 @@ export const textAlignment = (
   isVertical: boolean,
   position: VerticalAlignment | HorizontalAlignment,
 ) => {
-  console.log('TOUPPERCASEPROBLEM', position);
   const prefix = isVertical ? 'VERTICAL_' : '';
-  console.log('file: index.ts ~ line 13 ~ prefix', prefix);
   return cc[`${prefix}TEXT_ALIGNMENT_${position.toUpperCase()}`];
 };
 
